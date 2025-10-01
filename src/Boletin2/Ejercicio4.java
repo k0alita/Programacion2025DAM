d31bbe77956e4f795a4938556a227d6b8a2f9881
k0alita <alejandrohernandossan@gmail.com>
2025-10-01 14:43:30 +0200

    Nuevos Ejercicios Chat 1-5 Relacion 2

--- src/Boletin2/Ejercicio4.java
+++ src/Boletin2/Ejercicio4.java
@@ -9,15 +9,30 @@ public class Ejercicio4 {
         int numAleatorio = (int) (Math.random() * 100);
         System.out.println(numAleatorio);
 
+        while (num > 100 || num < 0) {
+            num = MiEntradaSalida.leerEntero("Introduce un numero entre 0 y 100");
+        }
+        int errores = 1;
         while (num < numAleatorio) {
             num = MiEntradaSalida.leerEntero("El numero aleatorio es mas grande, introduce otro numero: ");
+            errores++;
+            System.out.println("Tienes un total de: " + errores + " errores");
+            if (errores >= 5) {
+                System.out.println("Tienes un total de: " + errores + " errores, has perdido");
+                break;
+            }
         }
         while (num > numAleatorio) {
             num = MiEntradaSalida.leerEntero("El numero aleatorio es mas pequeño, introduce otro numero: ");
+            errores++;
+            System.out.println("Tienes un total de: " + errores + " errores");
+            if (errores >= 5) {
+                break;
+            }
         }
 
         if (num == numAleatorio) {
-            System.out.println("Has acertado el numero");
+            System.out.println("Has acertado el numero en: " + errores + " intentos");
         }
 
     }
