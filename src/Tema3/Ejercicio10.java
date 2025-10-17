e7468b60ab6dcfc7daa839f278d954082633f170
k0alita <alejandrohernandossan@gmail.com>
2025-10-17 10:14:00 +0200

    Nuevos Ejercicios Boletin 3

--- src/Tema3/Ejercicio10.java
+++ src/Tema3/Ejercicio10.java
@@ -3,13 +3,6 @@ package Tema3;
 public class Ejercicio10 {
     public static void main(String[] args) {
         int[] array = {1,1,1,2,3,4,4,4,5,6};
-        int duplicados = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 1){
-                duplicados++;
-            }
-        }
 
     }
-
 }
