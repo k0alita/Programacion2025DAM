1c0ba9c1854bb7e84320158191868560dabeda61
k0alita <alejandrohernandossan@gmail.com>
2025-10-22 10:15:16 +0200

    Nuevos Ejercicios Boletin 3

---
+++ src/BoletinMatrices/Ejercicio1.java
@@ -0,0 +1,16 @@
+//package BoletinMatrices;
+//
+//public class Ejercicio1 {
+//    public static void main(String[] args) {
+//        contieneNegativo({{1,2,3}, {4,5,6}});
+//    }
+//
+//    public static boolean contieneNegativo(int[][] matriz) {
+//
+//        for (int i = 0; i < matriz.length ; i++) {
+//            for (int j = 0; j < matriz[i].length; j++) {
+//                if ()
+//            }
+//        }
+//    }
+//}
