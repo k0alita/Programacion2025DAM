0dee1e251d837922cabab80b40e1465f3902353e
k0alita <alejandrohernandossan@gmail.com>
2025-10-28 12:42:56 +0100

    Nuevos Ejercicios Boletin 3

--- src/BoletinMatrices/Ejercicio8.java
+++ src/BoletinMatrices/Ejercicio8.java
@@ -1,41 +1,31 @@
-package BoletinMatrices;
-
-/*
-Crea un metodo que reciba dos matrices, que deben ser de igual tamaño, y sume el
-contenido de las posiciones en una tercera matriz, que será la que devuelva:
-| 1 2| + | 4 1| = | 5 3|
-| 4 6| | 1 0| | 5 6|
-
- */
-
-public class Ejercicio8 {
-    public static void main(String[] args) {
-        int[][] matriz = {{1, 2, 3}, {4, 5, 6}};
-        int[][] matriz2 = {{1, 2, 3}, {4, 5, 6}};
-
-        int sumaTotal = sumaDeMatriz(matriz, matriz2);
-
-        System.out.println("La suma total de la matriz es: " + sumaTotal);
-    }
-
-
-    public static int sumaDeMatriz(int[][] matriz, int[][] matriz2) {
-        int suma = 0;
-        int sumaTotal = 0;
-        for (int i = 0; i < matriz.length; i++) { //Recorrer filas y columnas de la matriz
-            for (int j = 0; j < matriz[i].length; j++) {
-                suma += matriz[i][j];
-            }
-        }
-        int suma2 = 0;
-        for (int i = 0; i < matriz2.length; i++) { //Recorrer filas y columnas de la matriz
-            for (int j = 0; j < matriz2[i].length; j++) {
-                suma2 += matriz2[i][j];
-            }
-        }
-
-        sumaTotal = suma + suma2;
-
-        return sumaTotal;
-    }
-}
+//package BoletinMatrices;
+//
+///*
+//Crea un metodo que reciba dos matrices, que deben ser de igual tamaño, y sume el
+//contenido de las posiciones en una tercera matriz, que será la que devuelva:
+//| 1 2| + | 4 1| = | 5 3|
+//| 4 6| | 1 0| | 5 6|
+//
+// */
+//
+//public class Ejercicio8 {
+//    public static void main(String[] args) {
+//        int[][] matriz1 = {{1, 2, 3}, {4, 5, 6}};
+//        int[][] matriz2 = {{1, 2, 3}, {4, 5, 6}};
+//
+//        sumaDeMatriz(sumarMatrices(matriz1, matriz2));
+//    }
+//
+//
+//    public static int sumarMatrices(int[][] matriz1, int[][] matriz2) {
+//        int[][] matrizResultado = new int[matriz1.length][matriz1[0].length];
+//        for (int i = 0; i < matriz1.length; i++) { //Recorrer filas y columnas de la matriz
+//            for (int j = 0; j < matriz1[i].length; j++) {
+//                matrizResultado[i][j] = matriz1[i][j] + matriz2[i][j];
+//            }
+//        }
+//        return matrizResultado;
+//    }
+//
+//    public static void sumarMatriz
+//}
---
+++ src/BoletinMatrices/Ejercicio9.java
@@ -0,0 +1,28 @@
+package BoletinMatrices;
+/*
+Crea un metodo que compruebe si una matriz pasada por parámetro es simétrica en
+función del eje X. Es decir, la matriz
+1 3 5 6
+4 2 0 8
+4 2 0 8
+1 3 5 6
+ */
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        int[][] numeros = {{1,3,5,6}, {4,2,0,8}, {4,2,0,8}, {1,3,5,6}};
+
+        System.out.println("La matriz es simetrica?: " + comprobarMatrizSimetrica(numeros));
+    }
+
+    public static boolean comprobarMatrizSimetrica(int[][] matriz) {
+        for (int i = 0; i <= matriz.length / 2; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] != matriz[matriz.length - 1 -i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
