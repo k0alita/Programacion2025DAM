0a61adf5da26711bf74b9974eeed5fa5933817a1
k0alita <alejandrohernandossan@gmail.com>
2025-10-30 13:44:17 +0100

    Ejercicio Jardin

---
+++ src/ExamenArrays/Ejercicio2.java
@@ -0,0 +1,74 @@
+package ExamenArrays;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int[][] jardin = {
+                {1, 0, 0, 0, 1, 0, 0, 0, 2, 1, 1, 0, 0, 5, 0},
+                {0, 2, 0, 5, 0, 0, 3, 0, 1, 0, 0, 0, 1, 0, 0},
+                {3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 4},
+                {0, 0, 2, 0, 0, 0, 0, 0, 2, 1, 3, 0, 0, 0, 1},
+                {0, 0, 1, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0, 3},
+                {4, 3, 0, 0, 0, 2, 1, 0, 0, 0, 0, 5, 0, 0, 0},
+                {1, 2, 0, 5, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                {3, 0, 0, 0, 0, 0, 2, 1, 4, 0, 0, 0, 1, 0, 0},
+                {2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 0},
+                {0, 0, 5, 1, 0, 0, 0, 5, 0, 3, 0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 2, 4, 0},
+                {0, 0, 1, 3, 0, 0, 1, 0, 0, 1, 0, 0, 1, 3, 0},
+                {0, 1, 2, 1, 0, 0, 0, 0, 0, 2, 3, 1, 0, 0, 1},
+                {1, 3, 4, 0, 0, 5, 0, 0, 1, 0, 0, 0, 0, 4, 2}
+        };
+
+        recorrerJardin(jardin);
+    }
+
+    public static void recorrerJardin(int[][] jardin) {
+        for (int i = 0; i < jardin.length; i++) {
+            for (int j = 0; j < jardin[i].length; j++) {
+                if (jardin[i][j] == 1) {
+                    if (florece(jardin,  i, j)) System.out.println("La flor de la posición " + i + "," + j + " florecerá");
+                }
+            }
+        }
+    }
+
+    public static boolean florece(int[][] jardin, int fila, int col) {
+        return hayAgua(jardin, fila, col);
+    }
+
+    public static boolean hayMalaHierba(int[][] jardin) {
+        for (int i = 0; i < jardin.length; i++) {
+            for (int j = 0; j < jardin[i].length; j++) {
+                if (jardin[i][j] == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean hayAbeja(int[][] jardin) {
+        for (int i = 0; i < jardin.length; i++) {
+            for (int j = 0; j < jardin[i].length; j++) {
+                if (jardin[i][j] == 5) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean hayAgua(int[][] jardin, int fila, int col) {
+        for (int i = fila - 1; i < fila + 1; i++) {
+            for (int j = fila -1; j < fila + 1; j++) {
+                if (j > 0 ) {
+                    if (jardin[i][j] == 2) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
